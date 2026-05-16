ce0bc15496cde4a29fdf0d08ec51d23b57fe6162
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-05-16 10:44:36 +0700

    setup config

--- config/index.js
+++ config/index.js
@@ -4,5 +4,7 @@ dotenv.config()
 
 module.exports = {
     binanceApiKeyTest: process.env.BINANCE_API_KEY_TEST,
-    binanceSecretKeyTest: process.env.BINANCE_SECRET_KEY_TEST
+    binanceSecretKeyTest: process.env.BINANCE_SECRET_KEY_TEST,
+    binaneApiKeyMain: process.env.BINANCE_API_KEY_MAIN,
+    binanceSecretKeyMain: process.env.BINANCE_SECRET_KEY_MAIN
 }
