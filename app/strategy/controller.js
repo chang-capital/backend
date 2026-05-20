737c89db4d201652aa702cd8382f592726b32491
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-05-20 14:19:27 +0700

    feat(calculateRSI): calculating RSI with technicalindicators

--- app/strategy/controller.js
+++ app/strategy/controller.js
@@ -1,3 +1,27 @@
+const { RSI } = require('technicalindicators')
+const { binanceApiKeyTest, binanceSecretKeyTest } = require('../../config')
+
+const test = new Spot(binanceApiKeyTest, binanceSecretKeyTest, {
+    baseURL: 'https://testnet.binance.vision'
+})
+
 module.exports = {
-    
+    calculateRsi: async(symbol, interval = '1h', period = 14) => {
+        // ambil candlestick
+        const klines = await test.klines(symbol, interval, { limit: period + 50 })
+
+        // ambil closing price
+        const closingPrices = klines.data.map(candle => parseFloat(candle[4]))
+
+        // hitung RSI
+        const rsiValues = RSI.calculate({
+            values: closingPrices,
+            period: period
+        })
+
+        // ambil RSI terakhir
+        const currentRSI = rsiValues[rsiValues.length - 1]
+
+        return parseFloat(currentRSI.toFixed(2))
+    }
 }
